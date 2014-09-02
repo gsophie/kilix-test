8f9089b34adbae937b3b4f64da913fcb1a1d221e
gsophie <takunette@gmail.com>
2014-09-02 10:31:49 +0200

    petit oubli...

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
           outputStyle: 'compressed'
         },
         files: {
-          'css/app.css': 'scss/app.scss'
+          'css/app.css': 'scss/app.scss',
           'css/my_css.css': 'scss/my_css.scss'
         }        
       }
